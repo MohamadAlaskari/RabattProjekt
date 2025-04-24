be9f9f6fd0b579fcc2fb4fcc5b7a746d3e282894
MohamadAlaskari <m_alskari96@hotmail.com>
2025-04-24 16:25:27 +0200

    implementiere Rabattberechnung basierend auf Alter und Mitgliedschaft schreibe Unit Tests für Rabattlogik mit Jest

--- rabatt.test.js
+++ rabatt.test.js
@@ -1,13 +1,29 @@
 const berechneRabatt = require('./rabatt');
 
-test('30 Jahre alt, kein Mitglied → 0%', () => {
+test('Alter 12, kein Mitglied → Rabatt 0%', () => {
+  expect(berechneRabatt(12, false)).toBe(0);
+});
+
+test('Alter 30, kein Mitglied → Rabatt 0%', () => {
   expect(berechneRabatt(30, false)).toBe(0);
 });
 
-test('15 Jahre alt, Mitglied → 35%', () => {
+test('Alter 30, Mitglied → Rabatt 15%', () => {
+  expect(berechneRabatt(30, true)).toBe(15);
+});
+
+test('Alter 15, kein Mitglied → Rabatt 20%', () => {
+  expect(berechneRabatt(15, false)).toBe(20);
+});
+
+test('Alter 65, kein Mitglied → Rabatt 30%', () => {
+  expect(berechneRabatt(65, false)).toBe(30);
+});
+
+test('Alter 15, Mitglied → Rabatt 35%', () => {
   expect(berechneRabatt(15, true)).toBe(35);
 });
 
-test('65 Jahre alt, Mitglied → 45%', () => {
+test('Alter 65, Mitglied → Rabatt 45%', () => {
   expect(berechneRabatt(65, true)).toBe(45);
 });
