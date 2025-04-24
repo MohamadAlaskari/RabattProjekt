58b05508306edb9346d5567c71bc8f98b48c1c6f
MohamadAlaskari <m_alskari96@hotmail.com>
2025-04-24 16:34:26 +0200

    arrow fuction

--- rabatt.js
+++ rabatt.js
@@ -1,20 +1,11 @@
-function berechneRabatt(age, isMember) {
-    if (age < 14) {
-      return 0;
-    } else if (age < 18 && isMember) {
-      return 35;
-    } else if (age < 18) {
-      return 20;
-    } else if (age >= 65 && isMember) {
-      return 45;
-    } else if (age >= 65) {
-      return 30;
-    } else if (isMember) {
-      return 15;
-    } else {
-      return 0;
-    }
-  }
-  
-  module.exports = berechneRabatt; 
-  
+const berechneRabatt = (age, isMember) => {
+  if (age < 14) return 0;
+  if (age < 18 && isMember) return 35;
+  if (age < 18) return 20;
+  if (age >= 65 && isMember) return 45;
+  if (age >= 65) return 30;
+  if (isMember) return 15;
+  return 0;
+};
+
+module.exports = berechneRabatt;
